01daad992b458bb7cfd327291c5c2010334bb884
AJ-Romera <ajromeraalfonso@gmail.com>
2021-03-15 19:25:19 +0100

    CHNG: Lee el State de Busqueda && Inicia Consulta a la API en el Submit

--- src/componentes/Formulario.js
+++ src/componentes/Formulario.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Formulario() {
+    const [busqueda, setBusqueda] = useState({
+        artista: '',
+        cancion: '',
+    });
+    const [error, setError] = useState(false);
+
+    const { artista, cancion } = busqueda;
+
+    const actualizarStateBusqueda = (e) => {
+        setBusqueda({
+            ...busqueda,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    // Consultar las APIs
+    const buscarBioYLetra = (e) => {
+        e.prevent.default();
+
+        if (artista.trim() === '' || cancion.trim() === '') {
+            setError(true);
+            return;
+        }
+        setError(false);
+    };
+
     return (
         <div className='bg-info'>
             <div className='container'>
                 <div className='row'>
-                    <form className='col card text-white bg-transparent mb-5 pt-5 pb-2'>
+                    <form
+                        className='col card text-white bg-transparent mb-5 pt-5 pb-2'
+                        onSubmit={buscarBioYLetra}
+                    >
                         <fieldset>
                             <legend className='text-center'>
                                 Buscador Bio de Artistas Musicales y Letras de
@@ -21,6 +50,8 @@ function Formulario() {
                                             className='form-control'
                                             name='artista'
                                             placeholder='Nombre Artista'
+                                            onChange={actualizarStateBusqueda}
+                                            value={artista}
                                         />
                                     </div>
                                 </div>
@@ -32,6 +63,8 @@ function Formulario() {
                                             className='form-control'
                                             name='cancion'
                                             placeholder='Nombre Canción'
+                                            onChange={actualizarStateBusqueda}
+                                            value={cancion}
                                         />
                                     </div>
                                 </div>
