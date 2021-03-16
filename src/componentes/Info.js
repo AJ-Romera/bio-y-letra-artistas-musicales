93d125992a1eca6ea72c25e475fe9ab22b2fdece
AJ-Romera <ajromeraalfonso@gmail.com>
2021-03-16 17:50:24 +0100

    CHNG: Añade Más Información de los Artistas

--- src/componentes/Info.js
+++ src/componentes/Info.js
@@ -32,7 +32,32 @@ function Info({ info }) {
             </div>
             <div className='card-body'>
                 <img src={strArtistThumb} alt='Imagen del Artista' />
-                <p className='card-text'>Género: {strGenre}</p>
+                <p className='card-text'>
+                    <b>Género: </b>
+                    {strGenre}
+                </p>
+                <p className='card-text'>
+                    <b>Ciudad/País: </b>
+                    {strCountry}
+                </p>
+                <p className='card-text'>
+                    <b>Marca/Empresa: </b>
+                    {strLabel}
+                </p>
+                <p className='card-text'>
+                    <b>Empezó en: </b>
+                    {intFormedYear}
+                </p>
+                <p className='card-text'>
+                    <b>En activo: </b>
+                    {strDisbanded === 'Yes'
+                        ? 'No'
+                        : strDisbanded === 'No' ||
+                          strDisbanded === '' ||
+                          strDisbanded === null
+                        ? 'Si'
+                        : 'No disponemos de dicha información en estos momentos'}
+                </p>
                 <h2 className='card-text'>Biografía:</h2>
                 <p className='card-text'>
                     {strBiographyES
@@ -42,34 +67,42 @@ function Info({ info }) {
                         : 'No hay biografía disponible en español ni inglés'}
                 </p>
                 <p className='card-text'>
-                    <a
-                        href={`https://${strWebsite}`}
-                        target='_blank'
-                        rel='noopener noreferrer'
-                    >
-                        <i className='fas fa-link'></i>
-                    </a>
-                    <a
-                        href={`https://${strFacebook}`}
-                        target='_blank'
-                        rel='noopener noreferrer'
-                    >
-                        <i className='fab fa-facebook'></i>
-                    </a>
-                    <a
-                        href={`https://${strTwitter}`}
-                        target='_blank'
-                        rel='noopener noreferrer'
-                    >
-                        <i className='fab fa-twitter'></i>
-                    </a>
-                    <a
-                        href={`${strLastFMChart}`}
-                        target='_blank'
-                        rel='noopener noreferrer'
-                    >
-                        <i className='fab fa-lastfm'></i>
-                    </a>
+                    {strWebsite ? (
+                        <a
+                            href={`https://${strWebsite}`}
+                            target='_blank'
+                            rel='noopener noreferrer'
+                        >
+                            <i className='fas fa-link'></i>
+                        </a>
+                    ) : null}
+                    {strFacebook ? (
+                        <a
+                            href={`https://${strFacebook}`}
+                            target='_blank'
+                            rel='noopener noreferrer'
+                        >
+                            <i className='fab fa-facebook'></i>
+                        </a>
+                    ) : null}
+                    {strTwitter ? (
+                        <a
+                            href={`https://${strTwitter}`}
+                            target='_blank'
+                            rel='noopener noreferrer'
+                        >
+                            <i className='fab fa-twitter'></i>
+                        </a>
+                    ) : null}
+                    {strLastFMChart ? (
+                        <a
+                            href={`${strLastFMChart}`}
+                            target='_blank'
+                            rel='noopener noreferrer'
+                        >
+                            <i className='fab fa-lastfm'></i>
+                        </a>
+                    ) : null}
                 </p>
             </div>
         </div>
